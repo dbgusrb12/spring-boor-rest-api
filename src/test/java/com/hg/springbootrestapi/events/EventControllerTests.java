0b832e689fc010e3f6688451b89c2aa68d0e8922
hyungyu <hg.yu@twolinecode.com>
2023-07-18 22:12:57 +0900

    feat: EventController 추가 및 test code 수정

--- src/test/java/com/hg/springbootrestapi/events/EventControllerTests.java
+++ src/test/java/com/hg/springbootrestapi/events/EventControllerTests.java
@@ -1,8 +1,12 @@
 package com.hg.springbootrestapi.events;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import java.time.LocalDateTime;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -17,13 +21,31 @@ public class EventControllerTests {
     @Autowired
     MockMvc mockMvc;
 
+    @Autowired
+    ObjectMapper objectMapper;
+
     @DisplayName("이벤트 생성 테스트")
     @Test
     void createEvent() throws Exception {
-        mockMvc.perform(
-            post("/api/events/")
+        Event event = Event.builder()
+            .name("Spring")
+            .description("REST API Development with Spring")
+            .beginEnrollmentDateTime(LocalDateTime.of(2023, 7, 18, 21, 59))
+            .closeEnrollmentDateTime(LocalDateTime.of(2023, 7, 19, 21, 59))
+            .beginEventDateTime(LocalDateTime.of(2023, 7, 20, 21, 59))
+            .endEventDateTime(LocalDateTime.of(2023, 7, 21, 21, 59))
+            .basePrice(100)
+            .maxPrice(200)
+            .limitOfEnrollment(100)
+            .location("강남역")
+            .build();
+
+        mockMvc.perform(post("/api/events/")
                 .contentType(MediaType.APPLICATION_JSON)
                 .accept(MediaTypes.HAL_JSON)
-        ).andExpect(status().isCreated());
+                .content(objectMapper.writeValueAsString(event)))
+            .andDo(print())
+            .andExpect(status().isCreated())
+            .andExpect(jsonPath("id").exists());
     }
 }
